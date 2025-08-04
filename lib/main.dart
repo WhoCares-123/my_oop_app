01153f9392ae4aec506650f06bd07afd5d191431
WhoCares-123 <torukrodriguez0@gmail.com>
2025-08-04 23:26:37 +0800

    chore: minor update to trigger PR comparison

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
- 
+ // minor update
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Hello OOP App',
       home: Scaffold(
         body: Center(
-          // Minor update to trigger pull request comparison
+          
           child: Text(
             'Hello, OOP!',
             style: TextStyle(fontSize: 24),
